c3c02cc6a9fc46c7edb6e7c007ead37bb8487712
UlisesPachecoMarinn <127709200+UlisesPachecoMarinn@users.noreply.github.com>
2024-04-24 21:02:16 -0600

    Add files via upload

---
+++ Pagina Web/Contacto.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta name="keywords"
+    content="Desarrollo web, Instalacion de redes, soporte tecnico, Administracion de base de datos, Servicios de redes, Diseño">
+  <meta name="description" content="Pagina Empresarial de Digital Cherry Mexico">
+  <meta name="author" content="Digital Cherry">
+  <link rel="stylesheet" href="css/bootstrap.min.css">
+  <link rel="stylesheet" href="css/main.css">
+  <link href="https://unpkg.com/aos@2.3.1/dist/aos.css" rel="stylesheet">
+
+  <script src="js/jquery.js"></script>
+
+  <title>Contacto| DigitalCherry</title>
+</head>
+
+<body>
+  <!--Navbar-->
+  <?php include 'include/navbar.php';?>
+  <!-- Terminar Navbar-->
+  <?php include 'include/contactos.php';?>
+
+  <script src="js/bootstrap.min.js"></script>
+  <script src="https://unpkg.com/aos@2.3.1/dist/aos.js"></script>
+  <script>
+    AOS.init();
+  </script>
+</body>
+
+</html>
---
+++ Pagina Web/somos.php
@@ -0,0 +1,227 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="keywords"
+    content="Desarrollo web, Instalacion de redes, soporte tecnico, Administracion de base de datos, Servicios de redes, Diseño">
+    <meta name="description" content="Pagina Empresarial de Digital Cherry Mexico">
+    <meta name="author" content="Digital Cherry">
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+    <link rel="stylesheet" href="css/main.css">
+    <link href="https://unpkg.com/aos@2.3.1/dist/aos.css" rel="stylesheet">
+
+    <script src="js/jquery.js"></script>
+
+    <title>Maqueta</title>
+</head>
+<body>
+    <!--nav-->
+    <?php include 'include/navbar.php';?>
+    <!--termina-->
+
+    <section id="Valores">
+      <div class="row">
+        <div class="col-sm-12 col-md-12 col-lg-12" >
+        <div class="row mt-2 py-2">
+          <img src="img/cherr3.jpeg" class="img-fluid" alt="">
+        </div>
+        </div>
+      </div>
+    </section>
+
+ 
+
+    <section id="Nosotros">
+        <div class="row">
+            <div class="col-sm-12 col-md-12 col-lg-12" >
+            <div class="row mt-5 py-5">
+    
+                <h2 class="text-secondary display-6 text-center text-danger font" style="font-family: 'Times New Roman', Times, serif; " style="color: rgb(128, 7, 45);">
+    
+                DIGITAL CHERRY'S
+                </h2>
+                
+                <p class="text-secondary text-center"
+                    style="font-family: 'Lucida Sans', 'Lucida Sans Regular', 'Lucida Grande', 'Lucida Sans Unicode', Geneva, Verdana, sans-serif;" bgsecondary>
+                    
+                    </p>
+                <h3 class="text-secondary display-6 text-center text-darck font"
+                style="font-family: 'Times New Roman', Times, serif; " >
+    
+                SOÑADO POR TI , CREADO POR NOSOTROS 
+                </h3>
+                <p class="text-secondary text-center"
+                    ></p>
+                    <p class="text-secondary text-c" bgsecondary>
+                    </p>
+                    <p class="text-secondary text-c" bgsecondary>
+                    </p>
+                    <p class="text-secondary text-c" bgsecondary>
+                    </p>
+    
+                <span class="text-center text-darck" style="font-family: Georgia, 'Times New Roman', Times, serif;" bgsecondary>Somos una empresa comprometida con sus clientes </span>
+            </div>
+            </div>
+        </div>
+        </section>
+        <section id="servicios">
+    <div class="container">
+        <div class="row mt-3">
+        <div class="col-sm-4 col-md-4 col-lg-4 mb-2" data-aos="fade-up">
+            <div class="card shadow">
+            <div class="row text-center"></div>
+            <h4 class="tex-danger text-center mt-4"
+            style="color: rgb(128, 7, 45);"
+                style="font-family: 'Times New Roman', Times, serif;">
+                Mision</h4>
+            <div class="row mt-2">
+                <p class="text-center" style="font-family:'Times New Roman', Times, serif;">Digital Cherry, tiene la
+                misión de ser la empresa líder en el mercado de la tecnología por su competitividad y productividad de
+                nuestros servicios.
+                </p>
+            </div>
+            </div>
+        </div>
+        <div class="col-sm-4 col-md-4 col-lg-4 mb-2" data-aos="fade-up">
+            <div class="card shadow">
+            <div class="row text-center"></div>
+            <h4 class="tex-danger text-center mt-4"
+            style="color: rgb(128, 7, 45);"
+                style="font-family: 'Times New Roman', Times, serif;">
+                Vision</h4>
+            <div class="row mt-2">
+                <p class="text-center" style="font-family:'Times New Roman', Times, serif;">
+                Ser una empresa capaz de cumplir aquellos sueños que surgen a través de las necesidades por medio de la
+                tecnología con las herramientas necesarias para satisfacer y cubrir aquellas necesidades.
+                </p>
+            </div>
+            </div>
+        </div>
+        <div class="col-sm-4 col-md-4 col-lg-4 mb-2" data-aos="fade-up">
+            <div class="card shadow">
+            <div class="row text-center"></div>
+            <h4 class="tex-danger text-center mt-4"
+            style="color: rgb(128, 7, 45);"
+                style="font-family: 'Times New Roman', Times, serif;">
+                Politica de Calidad </h4>
+            <div class="row mt-2">
+                <p class="text-center" style="font-family:'Times New Roman', Times, serif;">En Digital Cherry buscamos
+                asegurar la confidencialidad, integridad y disponibilidad , nos concentramos en las necesidades y
+                expectativas de cada una de las partes interesadas.</p>
+            </div>
+            </div>
+        </div>
+        
+        </div>
+    </div>
+    </section>
+    <section id="servicios">
+
+        <div class="row mt-3">
+          <div class="col-sm-3 col-md-6 col-lg-12 mb-2" data-aos="fade-up">
+            
+              <div class="row text-center"></div>
+              <h4 class="text-white text-center mt-4"
+              style="background-color: rgba(128, 0, 13, 0.822);"
+                style="font-family: 'Times New Roman', Times, serif;" >
+             DIGITAL CHERRY'S </h4>
+              
+              <div class="row mt-2">
+                
+            </div>
+          </div>
+        </div>
+    </section>
+    <section id="banner" class="banner mt-5">
+      <div class="container">
+        <div class="row justify-content-center">
+          <div class="col-sm-12 col-md-12 col-lg-12">
+            <div class="row">
+              <h3 class="text-center text-white fs-3 display-5 mt-5" style="font-family:Georgia, 'Times New Roman', Times, serif">REGISTRATE</h3>
+            </div>
+            <p class="text-darck text-center"
+            style="font-family: 'Times New Roman', Times, serif;" bgsecondary>
+            </p>
+            <p class="text-white text-center"
+            style="font-family:Georgia, 'Times New Roman', Times, serif" bgsecondary>
+            
+            BUSCANOS EN NUESTRAS REDES COMO: </p>
+          <div class="col-sm-12 col-md-12 col-lg-12">
+          <div class="row">
+                    <div class="col-sm-3 col-md-3 col-lg-3 mt-3" data-aos="fade-up">
+                        <div class="card shadow">
+                            <div class="row text-center"></div>
+                            <h4 class="tex-danger text-center mt-4" style="font-family:'Times New Roman', Times, serif; color: rgb(163, 31, 88);">
+                                <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="currentColor" class="bi bi-facebook" viewBox="0 0 16 16">
+                                    <path d="M16 8.049c0-4.446-3.582-8.05-8-8.05C3.58 0-.002 3.603-.002 8.05c0 4.017 2.926 7.347 6.75 7.951v-5.625h-2.03V8.05H6.75V6.275c0-2.017 1.195-3.131 3.022-3.131.876 0 1.791.157 1.791.157v1.98h-1.009c-.993 0-1.303.621-1.303 1.258v1.51h2.218l-.354 2.326H9.25V16c3.824-.604 6.75-3.934 6.75-7.951" />
+                                </svg>
+                            </h4>
+                            <div class="row mt-2">
+                                <p class="text-center" style="font-family: 'Times New Roman', Times, serif;" width="50" height="50">DigitalCherrys
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-sm-3 col-md-3 col-lg-3  mt-3" data-aos="fade-up">
+                        <div class="card shadow">
+                            <div class="row text-center"></div>
+                            <h4 class="tex-danger text-center mt-4" style="font-family:'Times New Roman', Times, serif; color: rgb(163, 31, 88);">
+                                <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="currentColor" class="bi bi-instagram" viewBox="0 0 16 16">
+                                    <path d="M8 0C5.829 0 5.556.01 4.703.048 3.85.088 3.269.222 2.76.42a3.9 3.9 0 0 0-1.417.923A3.9 3.9 0 0 0 .42 2.76C.222 3.268.087 3.85.048 4.7.01 5.555 0 5.827 0 8.001c0 2.172.01 2.444.048 3.297.04.852.174 1.433.372 1.942.205.526.478.972.923 1.417.444.445.89.719 1.416.923.51.198 1.09.333 1.942.372C5.555 15.99 5.827 16 8 16s2.444-.01 3.298-.048c.851-.04 1.434-.174 1.943-.372a3.9 3.9 0 0 0 1.416-.923c.445-.445.718-.891.923-1.417.197-.509.332-1.09.372-1.942C15.99 10.445 16 10.173 16 8s-.01-2.445-.048-3.299c-.04-.851-.175-1.433-.372-1.941a3.9 3.9 0 0 0-.923-1.417A3.9 3.9 0 0 0 13.24.42c-.51-.198-1.092-.333-1.943-.372C10.443.01 10.172 0 7.998 0zm-.717 1.442h.718c2.136 0 2.389.007 3.232.046.78.035 1.204.166 1.486.275.373.145.64.319.92.599s.453.546.598.92c.11.281.24.705.275 1.485.039.843.047 1.096.047 3.231s-.008 2.389-.047 3.232c-.035.78-.166 1.203-.275 1.485a2.5 2.5 0 0 1-.599.919c-.28.28-.546.453-.92.598-.28.11-.704.24-1.485.276-.843.038-1.096.047-3.232.047s-2.39-.009-3.233-.047c-.78-.036-1.203-.166-1.485-.276a2.5 2.5 0 0 1-.92-.598 2.5 2.5 0 0 1-.6-.92c-.109-.281-.24-.705-.275-1.485-.038-.843-.046-1.096-.046-3.233s.008-2.388.046-3.231c.036-.78.166-1.204.276-1.486.145-.373.319-.64.599-.92s.546-.453.92-.598c.282-.11.705-.24 1.485-.276.738-.034 1.024-.044 2.515-.045zm4.988 1.328a.96.96 0 1 0 0 1.92.96.96 0 0 0 0-1.92m-4.27 1.122a4.109 4.109 0 1 0 0 8.217 4.109 4.109 0 0 0 0-8.217m0 1.441a2.667 2.667 0 1 1 0 5.334 2.667 2.667 0 0 1 0-5.334" />
+                                </svg>
+                            </h4>
+                            <div class="row mt-2">
+                                <p class="text-center" style="font-family: 'Times New Roman', Times, serif;" width="50" height="50">
+                                    Digital_Cherry33
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="col-sm-3 col-md-3 col-lg-3 mt-3" data-aos="fade-up">
+                        <div class="card shadow">
+                            <div class="row text-center"></div>
+                            <h4 class="tex-danger text-center mt-4" style="font-family:'Times New Roman', Times, serif; color: rgb(163, 31, 88);">
+                                <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="currentColor" class="bi bi-whatsapp" viewBox="0 0 16 16">
+                                    <path d="M13.601 2.326A7.85 7.85 0 0 0 7.994 0C3.627 0 .068 3.558.064 7.926c0 1.399.366 2.76 1.057 3.965L0 16l4.204-1.102a7.9 7.9 0 0 0 3.79.965h.004c4.368 0 7.926-3.558 7.93-7.93A7.9 7.9 0 0 0 13.6 2.326zM7.994 14.521a6.6 6.6 0 0 1-3.356-.92l-.24-.144-2.494.654.666-2.433-.156-.251a6.56 6.56 0 0 1-1.007-3.505c0-3.626 2.957-6.584 6.591-6.584a6.56 6.56 0 0 1 4.66 1.931 6.56 6.56 0 0 1 1.928 4.66c-.004 3.639-2.961 6.592-6.592 6.592m3.615-4.934c-.197-.099-1.17-.578-1.353-.646-.182-.065-.315-.099-.445.099-.133.197-.513.646-.627.775-.114.133-.232.148-.43.05-.197-.1-.836-.308-1.592-.985-.59-.525-.985-1.175-1.103-1.372-.114-.198-.011-.304.088-.403.087-.088.197-.232.296-.346.1-.114.133-.198.198-.33.065-.134.034-.248-.015-.347-.05-.099-.445-1.076-.612-1.47-.16-.389-.323-.335-.445-.34-.114-.007-.247-.007-.38-.007a.73.73 0 0 0-.529.247c-.182.198-.691.677-.691 1.654s.71 1.916.81 2.049c.098.133 1.394 2.132 3.383 2.992.47.205.84.326 1.129.418.475.152.904.129 1.246.08.38-.058 1.171-.48 1.338-.943.164-.464.164-.86.114-.943-.049-.084-.182-.133-.38-.232" />
+                                </svg>
+                            </h4>
+                            <div class="row mt-2">
+                                <p class="text-center" style="font-family: 'Times New Roman', Times, serif;" width="50" height="50">5566215470</p>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-sm-3 col-md-3 col-lg-3 mb-2 mt-3" data-aos="fade-up">
+                        <div class="card shadow">
+                            <div class="row text-center"></div>
+                            <h4 class="tex-danger text-center mt-4" style="font-family: 'Times New Roman', Times, serif; color: rgb(163, 31, 88);">
+                                <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="currentColor" class="bi bi-envelope" viewBox="0 0 16 16">
+                                    <path d="M0 4a2 2 0 0 1 2-2h12a2 2 0 0 1 2 2v8a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2zm2-1a1 1 0 0 0-1 1v.217l7 4.2 7-4.2V4a1 1 0 0 0-1-1zm13 2.383-4.708 2.825L15 11.105zm-.034 6.876-5.64-3.471L8 9.583l-1.326-.795-5.64 3.47A1 1 0 0 0 2 13h12a1 1 0 0 0 .966-.741M1 11.105l4.708-2.897L1 5.383z" />
+                                </svg>
+                            </h4>
+                            <div class="row mt-2">
+                                <p class="text-center" width="50" height="50">Digitalcherrys@gmail.com</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+  
+          </div>
+        </div>
+      </div>
+    </section>
+
+
+
+    <script src="js/bootstrap.min.js"></script>
+    <script src="https://unpkg.com/aos@2.3.1/dist/aos.js"></script>
+    <script>
+        AOS.init();
+    </script>
+    </body>
+    
+    </html>
